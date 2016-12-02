a138fc94fceacb858cb3bc175be92204e8a45482
Dr. Hazem Ali <y3m@hotmail.com>
2016-12-02 10:19:29 +0200

    Update Test.php

--- Test.php
+++ Test.php
@@ -22,7 +22,8 @@
 
   //--------------------------------------------//
   /* Encrypt String Test */
-    echo $Encryptor->Encrypt("Hello World");
+    $EncryptedString =  $Encryptor->Encrypt("Hello World");
+    echo "ENCRYPTED STRING :> {$EncryptedString} \r\n";
   //--------------------------------------------//
 
 
@@ -30,5 +31,26 @@
   /* Decrypt String Test
   * PLEASE DO NOT PERFORM THIS CALL IF THE PASSED
   * PARAMETER IS NOT ENCRYPTED */
-    echo $Encryptor->Encrypt("Encrypted Hash");
+    $EncryptedString = "WDBmZlZodU4zd2hCNkpqRjNCOXhndz09";
+    $DecryptedString =  $Encryptor->Decrypt($EncryptedString);
+    echo "DECRYPTED STRING :> {$DecryptedString} \r\n";
   //--------------------------------------------//
+
+
+
+  /* --------------- PASSWORD ENCRYPTION ---------------*
+  * THE ENCRYPTION CLASS DESIGNED TO PERFORM THE MAX.
+  * ENCRYPTION FOR PASSWORDS, WHICH CANNOT BE DECRYPTED
+  * IN ORDER TO ENCRYPT A PASSWORD, PLEASE CALL THIS FUNC.
+  */
+  $Password = "123456";
+  $EncryptedPassword = $Encryptor->EncryptPassword($Password);
+  echo "ENCRYPTED PASSWORD :> {$EncryptedPassword} \r\n";
+
+  /* YOU CAN PERFORM THE PASSWORD EQUALITY (IF STATEMENT)
+   * by using the condition below : */
+   if ($Encryptor->EncryptPassword($Password) == $EncryptedPassword)
+    {
+      // Password is Correct ....
+    }else { /* Password is not correct. */ }
+  // --------------------------------------------------//
